bec43125de24e60c2b314ff0fd25bcc513011541
Lazyreykis <lazyreykis@mail.ru>
2023-11-22 16:30:03 +0300

    My Hello World!

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Clearing {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Пиздец нахуй блять"); //My hello world!
+        }
+    }
+}
